47bba5e0b07565a8097ed871dff64773aa65ac2e
granadalook <tiven17.jsga@gmail.com>
2022-11-28 19:05:18 -0500

    CONFIG:Se agrega prfijo a la configuracion de la URL

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './Main/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1'); // Prefijo de url
   await app.listen(4000); // Escuchando por el puerto 4000
 }
 bootstrap();
