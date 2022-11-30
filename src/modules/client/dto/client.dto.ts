08a97935ca2152ab365175fa7d35b39cce489c41
granadalook <tiven17.jsga@gmail.com>
2022-11-29 19:48:54 -0500

    ADD/Se crea la entidad  y el dto para  la tabla de client

---
+++ src/modules/client/dto/client.dto.ts
@@ -0,0 +1,27 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsPhoneNumber,
+  IsString,
+} from 'class-validator';
+
+export class CreateClientDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'NOMBRE DEL CLIENTE' })
+  readonly name: string;
+  @IsEmail()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'EMAIL DEL CLIENTE' })
+  readonly email: string;
+  @IsPhoneNumber()
+  @IsNotEmpty()
+  @ApiProperty({ description: 'TELEFONO DEL CLIENTE' })
+  readonly phone: string;
+  @IsNotEmpty()
+  @IsNumber()
+  @ApiProperty({ description: 'ESTADO  DEL CLIENTE' })
+  readonly state: number;
+}
