143395f7579dad3ba9bb74fa882442a43cd6087a
granadalook <tiven17.jsga@gmail.com>
2022-12-01 16:38:38 -0500

    ADD:Se crean metodo para traer  todos los clients

--- src/modules/client/controller/client/client.controller.ts
+++ src/modules/client/controller/client/client.controller.ts
@@ -1,11 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ClientService } from '../../services/client/client.service';
 @ApiTags('CLIENT')
 @Controller('client')
 export class ClientController {
+  constructor(private clientService: ClientService) {}
+
   @Get()
-  @ApiOperation({ summary: 'CLIENT' })
-  getHello(): string {
-    return 'CLIENT CONTROLLER';
+  @ApiOperation({ summary: 'TRAE TODOS LOS CLIENTS ' })
+  getAllClients() {
+    return this.clientService.getAllClients();
   }
 }
--- src/modules/client/services/client/client.service.ts
+++ src/modules/client/services/client/client.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Client } from '../../entity/client.entity';
@@ -8,4 +8,12 @@ export class ClientService {
   constructor(
     @InjectRepository(Client) private clientRepo: Repository<Client>,
   ) {}
+
+  async getAllClients() {
+    const clients = await this.clientRepo.find();
+    if (clients.length === 0) {
+      throw new NotFoundException(`No existen clientes`);
+    }
+    return clients;
+  }
 }
